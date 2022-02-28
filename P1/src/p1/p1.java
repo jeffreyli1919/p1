6a1f22eeacf6fa7db3c0ae6e60ceaf2cddc017ed
1855197 <1855197@01-19782.pusd.dom>
2022-02-28 09:01:18 -0800

    finding cake location

--- P1/src/p1/p1.java
+++ P1/src/p1/p1.java
@@ -10,12 +10,12 @@ public class p1 {
 	private static int rows;
 	private static int cols;
 	private static int rooms;
-	static long start = System.currentTimeMillis();
+	static long start;
 	static String[][] map1Coords;
 	
 	public static void main(String[] args) {
 		Scanner scanner = null;
-		File f = new File("JeffMap1Coord.txt");
+		File f = new File("JeffMap3Coord.txt");
 		
 		try {
 			//code that might throw a special error
@@ -29,41 +29,48 @@ public class p1 {
 			//use next methods to grab
 			
 			int currentRow = 0;
-			char[][] map1 = new char[rows][cols];
-			char[][] map2 = new char[rows][cols];
+			String[][] map1 = new String[rows][cols];
+			String[][] map2 = new String[rows][cols];
 			
 			scanner.nextLine();
 			
-			
 			//take in the cols and # of rooms
+			
+			
+			start = System.currentTimeMillis();
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				if (currentRow < rows) {
 					for (int i = 0; i < line.length(); i++) {
 						System.out.print(line.charAt(i));
+						
 						map1Coords[currentRow][i] = line.substring(0, 1);
-						map1[currentRow][i] = line.charAt(i);
+						map1[currentRow][i] = line.substring(i, i+1);
 					}
 				} else if (currentRow >= rows) {
 					for (int i = 0; i < line.length(); i++) {
 						System.out.print(line.charAt(i));
-						map2[currentRow-rows][i] = line.charAt(i);
+						map2[currentRow][i] = line.substring(i, i+1);
 					}
 				}
 				System.out.println();
 				currentRow++;
-				//System.out.println("currentRow: " + currentRow);
 				//use charAt to grab the elements of the map for a given row
 			}
 			System.out.println("runtime: " + (System.currentTimeMillis()-start));
 			
 			//printing out map1
-			for (int i = 0; i < rows; i++) {
-				System.out.println();
-				for (int j = 0; j < cols; j++) {
-					System.out.print(map1[i][j] + "");
-				}
-			}
+//			for (int i = 0; i < rows; i++) {
+//				System.out.println();
+//				for (int j = 0; j < cols; j++) {
+//					System.out.print(map1[i][j] + "");
+//				}
+//			}
+			
+			queueCakeLocation(scanner, map1);
+			
+			
+			
 			
 			
 			
@@ -72,7 +79,7 @@ public class p1 {
 			//System.out.println(e);
 			System.out.println();
 			coordinateBased(scanner);
-			queueSolution(scanner);
+			queueCakeLocation(scanner, map1Coords);
 			
 			
 			
@@ -94,12 +101,15 @@ public class p1 {
 				Integer curCol = Integer.valueOf(line.substring(4, 5));
 				map1Coords[curRow][curCol] = symbol;
 				System.out.println(symbol + " " + curRow + " " + curCol);
+			
 				
 				
 			}
 			System.out.println("runtime: " + (System.currentTimeMillis()-start));
 			
 			//printing out 2d array map1coords
+			System.out.println();
+			System.out.println("map1Coords: ");
 			for (int i = 0; i < rows; i++) {
 				System.out.println();
 				for (int j = 0; j < cols; j++) {
@@ -116,87 +126,91 @@ public class p1 {
 		
 	}
 	
-	public static void queueSolution(Scanner scan) {
+	public static void queueCakeLocation(Scanner scan, String[][] map) {
 		try {
 			//solution for coord based
 			//finding kirby and possible spaces for kirby
 			
 			
-			long start = System.currentTimeMillis();
-			
+			long locationStart = System.currentTimeMillis();
 			Queue<Integer> queue = new LinkedList<>();
+			int cakeRow = 0;
+			int cakeCol = 0;
+			
 			for (int i = 0; i < rows; i++) {
 				for (int j = 0; j < cols; j++) {
-					if (map1Coords[i][j].equals(".")) {
-						map1Coords[i][j] = "1";
-					} else if (map1Coords[i][j].equals("K")) {
+					if (map[i][j].equals(".")) {
+						map[i][j] = "1";
+					} else if (map[i][j].equals("K")) {
 						queue.add(i);
 						queue.add(j);
 					} 
 				}
 			}
-			
-			for (int i = 0; i < rows; i++) {
-				System.out.println();
-				for (int j = 0; j < cols; j++) {
-					System.out.print(map1Coords[i][j]);
-				}
-			}
+
 			
 			while (queue.size() > 0) {
-				int curY = queue.remove(); //3
-				int curX = queue.remove(); //0
-				System.out.println(curX + " " + curY);
-				//System.out.println();
-				//System.out.println(curX);
-				//System.out.println(curY);
-				//System.out.println(rows);
-				//System.out.println(cols);
-				
+				int curRow = queue.remove(); //x
+				int curCol = queue.remove(); //y
+				System.out.println(curRow + " " + curCol);
 			
+				map[curRow][curCol] = "-1";
 				
-				if (map1Coords[curX][curY].equals("C")) {
-					System.out.print("found");
-				}
-				
-				map1Coords[curX][curY] = "-1";
-				
-				if (curY > 0) {
-					if (map1Coords[curY-1][curX].equals("1")) {
-						queue.add(curY-1);
-						queue.add(curX);
+				if (curCol > 0) {
+					if (map[curRow][curCol-1].equals("C")) {
+						cakeRow = curRow;
+						cakeCol = curCol - 1;
+						System.out.print("found");
+						break;
+					} else if (map[curRow][curCol-1].equals("1")) {
+						queue.add(curRow);
+						queue.add(curCol-1);
+						
 					}
 				}
-				if (curY < cols-1) {
-					if (map1Coords[curY+1][curX].equals("1")) {
-						queue.add(curY+1);
-						queue.add(curX);
-					}
-				}
-				
-				if (curX < rows-1) {
-					//rows are y cols are x
-					if (map1Coords[curY][curX+1].equals("1")) {
-						queue.add(curY);
-						queue.add(curX+1);
-						//System.out.println("added");
+				if (curCol < cols-1) {
+					if (map[curRow][curCol+1].equals("C")) {
+						cakeRow = curRow;
+						cakeCol = curCol+1;
+						System.out.print("found");
+						break;
+					} else if (map[curRow][curCol + 1].equals("1")) {
+						queue.add(curRow);
+						queue.add(curCol+1);
 					}
 				}
 				
-				if (curX > 0) {
-					if (map1Coords[curY][curX-1].equals("1")) {
-						queue.add(curY);
-						queue.add(curX-1);
+				if (curRow < rows-1) {
+					if (map[curRow+1][curCol].equals("C")) {
+						cakeRow = curRow+1;
+						cakeCol = curCol;
+						System.out.print("found");
+						break;
+					}else if (map[curRow+1][curCol].equals("1")) {
+						queue.add(curRow+1);
+						queue.add(curCol);
+					}
+				}
+				
+				if (curRow > 0) {
+					if (map[curRow-1][curCol].equals("C")) {
+						cakeRow = curRow-1;
+						cakeCol = curCol;
+						System.out.print("found");
+						break;
+					} else if (map[curRow-1][curCol].equals("1")) {
+						queue.add(curRow-1);
+						queue.add(curCol);
 					}
 				}
 				
 				//System.out.println("end");
-				System.out.println(queue.size());
+				System.out.println("size: " + queue.size());
 			}
 			
 			System.out.println();
-			System.out.println("runtime: " + (System.currentTimeMillis()-start));
-			
+			System.out.println("Cake Row: "  + cakeRow + " Cake Col: " + cakeCol);
+			System.out.println("location runtime: " + (System.currentTimeMillis()-locationStart));
 			
 		} catch(Exception e) {
 			System.out.println();
