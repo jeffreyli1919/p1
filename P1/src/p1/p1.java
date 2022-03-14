5d9ba2ea25d666f4139a6a84da26e9525746bbb3
jeffreyli1919 <70667800+jeffreyli1919@users.noreply.github.com>
2022-03-14 00:38:02 -0700

    finalized p1 (before autograder)

--- P1/src/p1/p1.java
+++ P1/src/p1/p1.java
@@ -9,22 +9,30 @@ import java.util.Stack;
 
 public class p1 {
 	
-	private static int rows;
-	private static int cols;
-	private static int rooms;
+	private static int rows, cols, rooms;
 	static long start;
 	static String[][] mapCoords;
-	private static boolean coordBased;
+	private static boolean coordBasedInput, coordBasedOutput, legalInput, isSolution, queueBased;
 	private static Queue<Integer> queue = new LinkedList<>();
+	private static Stack<Integer> stack = new Stack<>();
 	private static ArrayList<Integer> dequeue = new ArrayList<>();
-	
 	public static void main(String[] args) {
 		Scanner scanner = null;
-		File f = new File("JeffMap2Coord.txt");
-		coordBased = false;
+		File f = new File("JeffMap6Coord.txt");
+		//can change these based on if you want to solve using stack/queue approach and desired output
+		queueBased = true;
+		coordBasedOutput = false;
+		//can't change these
+		coordBasedInput = false;
+		legalInput = true;
+		isSolution = false;
+		
 		
 		try {
 			//code that might throw a special error
+			//initializing stuff
+			//map1 is used if regular input
+			//mapCoords is used if coord input
 			scanner = new Scanner(f);
 			rows = scanner.nextInt();
 			cols = scanner.nextInt();
@@ -39,70 +47,80 @@ public class p1 {
 			start = System.currentTimeMillis();
 			
 		
-			//determine if input map is coordinate or regular
+			//determine if input map is coordinate or regular by simply looking at line 1
 			String first = scanner.nextLine();
 			if (first.substring(2, 3).equals("0")) {
-				coordBased = true;
+				coordBasedInput = true;
 				mapCoords[0][0] = first.substring(0, 1);
-				//System.out.print(mapCoords[0][0]);
 			} else {
 				for (int i = 0; i < first.length(); i++) {
 					map1[currentRow][i] = first.substring(i, i+1);
-					//System.out.print(first.charAt(i));
 				}
 				currentRow++;
 			}
 			
-			if (coordBased == false) {
+			if (coordBasedInput == false) {
 				//if regular input map fill in map1
-				while (scanner.hasNextLine() && coordBased == false) {
+				while (scanner.hasNextLine() && legalInput) {
 					String line = scanner.nextLine();
 					if (currentRow < rows*rooms) {
 						for (int i = 0; i < cols; i++) {
-							map1[currentRow][i] = line.substring(i, i+1);
+							if (line.substring(i, i+1).equals(".") || line.substring(i, i+1).equals("C") || line.substring(i, i+1).equals("@") || line.substring(i, i+1).equals("K")
+									|| line.substring(i, i+1).equals("|")) {
+								map1[currentRow][i] = line.substring(i, i+1);
+							} else { //checking for illegal characters
+								legalInput = false;
+								System.out.println("Error: illegal character");
+								break;
+							}
 						}
 					}
 					currentRow++;
 					//use charAt to grab the elements of the map for a given row
 				}
 				
-				
-				System.out.println("runtime: " + (System.currentTimeMillis()-start));
-				
-				
-				
-				//printing coord based map with regular map input
-				for (int i = 0; i < rooms; i++) {
-					for (int j = i*rows; j < rows*(i+1); j++) {
-						for (int k = 0; k < map1[0].length; k++) {
-							System.out.println(map1[j][k] + " " + (j - (rows*i)) + " " + k);
+				//printing runtime and original regular input map
+				if (legalInput) {
+					System.out.println("runtime: " + (System.currentTimeMillis()-start));
+					for (int i = 0 ; i < map1.length; i++) {
+						for (int j = 0 ; j < map1[0].length; j++) {
+							System.out.print(map1[i][j]);
 						}
+						System.out.println();
 					}
-				}
-				
-				for (int i = 0 ; i < map1.length; i++) {
-					for (int j = 0 ; j < map1[0].length; j++) {
-						System.out.print(map1[i][j]);
-					}
-					System.out.println();
 				}
 			}
 			
+			/* this prints out the coords of a regular based map to make a coord-based input map
+			 * of the regular map input
+			for (int i = 0; i < rooms; i++) {
+				for (int j = i*rows; j < rows*(i+1); j++) {
+					for (int k = 0; k < map1[0].length; k++) {
+						System.out.println(map1[j][k] + " " + (j - (rows*i)) + " " + k);
+					}
+				}
+			}
+			*/
 			
 			//solution if input is coordinate based
-			if (coordBased) {
+			if (coordBasedInput) {
 				coordinateBased(scanner);
-				for (int i = 1; i <= rooms; i++) {
-					queueCakeLocation(scanner, mapCoords, i);
+				if (legalInput) {
+					for (int i = 1; i <= rooms; i++) {
+						 if (queueBased)  {queueCakeLocation(scanner, mapCoords, i); }
+						 else { stackCakeLocation(scanner, mapCoords, i); }
+					}
 				}
-				
 			}
 			
 			
 			//solution if input is regular
-			if (!coordBased) {
-				for (int i = 1; i <= rooms; i++) {
-					queueCakeLocation(scanner, map1, i);
+			if (!coordBasedInput) {
+				if (legalInput) {
+					for (int i = 1; i <= rooms; i++) {
+						if (queueBased) queueCakeLocation(scanner, map1, i);
+						 else { stackCakeLocation(scanner, map1, i); }
+					}
 				}
 			}	
 			
@@ -115,22 +133,23 @@ public class p1 {
 	public static void coordinateBased(Scanner scan) {
 		
 		try {
-			
-			
-			
-			//Integer.valueOf(null) to convert from strings to ints
+			//insantiate stuff
 			int currentRoom = 0;
 			int col = 1;
 			int row = 0;
 			Integer curRow = 0;
 			Integer curCol = 0;
 			
+			//storing all values in coord based map into 2d array
 			while (scan.hasNextLine()) {
+				
+				//when column reaches the last col move to next row and reset col
 				if (col >= cols) {
 					col = 0;
 					row++;
 				} 
 				
+				//when row reaches last row move to next room and reset row and col
 				if (row >= rows) {
 					row = 0;
 					col = 0;
@@ -140,6 +159,7 @@ public class p1 {
 				String line = scan.nextLine();
 				String symbol = line.substring(0, 1);
 				
+				//determing what to scan based off of whether row and col are single digit or double digit
 				if (row >= 10 && col < 10) {
 					curRow = Integer.valueOf(line.substring(2, 4));
 					curCol = Integer.valueOf(line.substring(5, 6));
@@ -153,45 +173,56 @@ public class p1 {
 					curRow = Integer.valueOf(line.substring(2, 3));
 					curCol = Integer.valueOf(line.substring(4, 5));
 				}
-
+				//increasing column for the next coordinate
 				col++;
-				mapCoords[curRow+(currentRoom*rows)][curCol] = symbol;
-				//System.out.println(symbol + " " + curRow + " " + curCol);
-				//System.out.println("row: " + row + " col: " + col);
 				
-			
-				
+				//checking for invalid symbols
+				if (symbol.equals(".") || symbol.equals("K")|| symbol.equals("C") || symbol.equals("@") || symbol.equals("|")) {
+					mapCoords[curRow+(currentRoom*rows)][curCol] = symbol;
+				} else {
+					legalInput = false;
+					System.out.println("Error: illegal character");
+				}
 				
 			}
+			//print runtime and the map for coord based input
 			System.out.println("runtime: " + (System.currentTimeMillis()-start));
-			
-			//printing out 2d array map1coords
 			System.out.println();
 			System.out.println("map1Coords: ");
+			boolean complete = true;
 			for (int i = 0; i < rows*rooms; i++) {
-				System.out.println();
 				for (int j = 0; j < cols; j++) {
-					System.out.print(mapCoords[i][j]);
+					//checking for missing coordinates
+					if (mapCoords[i][j] == null) {
+						System.out.println();
+						legalInput = false;
+						complete = false;
+						System.out.println("Error: incomplete map");
+						break;
+					}
+					if (complete) {
+						System.out.print(mapCoords[i][j]);
+					}
 				}
+				System.out.println();
 			}
 			
 			System.out.println();
 			
 		} catch(Exception e) {
-			System.out.println(e);
-			System.out.println("exception catch fail: coordbased");
+			legalInput = false;
+			System.out.println("Error: Coordinate does not fit within maze");
 		}
 		
 	}
-	
+	//this is the approach to finding the cake using a queue, called once for each room
 	public static void queueCakeLocation(Scanner scan, String[][] map, int currentRoom) {
 		try {
-
 			long locationStart = System.currentTimeMillis();
 			int cakeRow = -1;
 			int cakeCol = -1;
-			
 			//turning available paths . into 1 and finding initial location
+			//checking for a door or cake to determine solution is potentially possible
 			for (int i = rows*(currentRoom-1); i < rows*currentRoom; i++) {
 				for (int j = 0; j < cols; j++) {
 					if (map[i][j].equals(".")) {
@@ -199,16 +230,26 @@ public class p1 {
 					} else if (map[i][j].equals("K")) {
 						queue.add(i);
 						queue.add(j);
-					} 
+					} else if (map[i][j].equals("|") || map[i][j].equals("C")) {
+						isSolution = true;
+						cakeRow = i;
+						cakeCol = j;
+					}
 				}
 			}
+			//if no cake or door is present
+			if (isSolution == false) {
+				System.out.println("The Cake is a Lie.");
+			}
 			
-			while (queue.size() > 0) {
+			//adding and removing moves from queue in order of North South East West
+			//if the cake if found the loop stops
+			//moves are stored in dequeue
+			while (queue.size() >= 2) {
 				int curRow = queue.remove(); //x
 				int curCol = queue.remove(); //y
 				dequeue.add(curRow);
 				dequeue.add(curCol);
-			
 				if (curRow > (currentRoom-1)*rows) {
 					if (map[curRow-1][curCol].equals("C") || map[curRow-1][curCol].equals("|")) {
 						cakeRow = curRow-1;
@@ -255,33 +296,20 @@ public class p1 {
 				}
 			}
 			
-			System.out.println();
-			
-			//finding optimal path
-			int index = dequeue.size();
-			int curRow = cakeRow;
-			int curCol = cakeCol;
-			while (index >= 2) {
-				int tempRow1 = dequeue.get(index-2);
-				int tempCol1 = dequeue.get(index-1);
-				//System.out.println(curRow + " " + curCol + " " + tempRow1 + " " + tempCol1);
-				if (curRow == tempRow1 && curCol == tempCol1-1 || curRow == tempRow1 && curCol == tempCol1+1 ||
-						curRow == tempRow1+1 && curCol == tempCol1 || curRow == tempRow1-1 && curCol == tempCol1) {
-					curRow = tempRow1;
-					curCol = tempCol1;
-				} else {
-					dequeue.remove(index-1);
-					dequeue.remove(index-2);
+			//finding optimal paths if cake or door exists within the room
+			if (isSolution) {
+				findOptimalPath(map, cakeRow, cakeCol);
+			}
+			//printing out the solution map if there is an optimal path within every room
+			if (isSolution) {
+				if (currentRoom == rooms) {
+					System.out.println("solution runtime: " + (System.currentTimeMillis()-locationStart));
+					if (!coordBasedOutput) {
+						printSolution(map);
+					}
+					
 				}
-				index -= 2;
 			}
-			
-			findOptimalPath(map);
-			
-			if (currentRoom == rooms) {
-				printSolution(map);
-			}
-			System.out.println("solution runtime: " + (System.currentTimeMillis()-locationStart));
 			
 		} catch(Exception e) {
 			System.out.println();
@@ -289,16 +317,155 @@ public class p1 {
 		}
 	}
 	
-	public static void findOptimalPath(String map[][]) {
-		
-		//printing optimal coords
-		for (int i = 2; i < dequeue.size(); i+=2) {
-			int temp = dequeue.get(i);
-			int temp2 = dequeue.get(i+1);
-			//System.out.println("row: " + temp + "col: " + temp2);
-			map[temp][temp2] = "+";
+	//this is the approach to finding the cake using a stack, called once for each room
+	public static void stackCakeLocation(Scanner scan, String[][] map, int currentRoom) {
+		try {
+			long locationStart = System.currentTimeMillis();
+			int cakeRow = -1;
+			int cakeCol = -1;
+			//turning available paths . into 1 and finding initial location of Kirby
+			//checking for a door or cake to determine solution is potentially possible
+			for (int i = rows*(currentRoom-1); i < rows*currentRoom; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (map[i][j].equals(".")) {
+						map[i][j] = "1";
+					} else if (map[i][j].equals("K")) {
+						stack.push(i);
+						stack.push(j);
+					} else if (map[i][j].equals("|") || map[i][j].equals("C")) {
+						isSolution = true;
+						cakeRow = i;
+						cakeCol = j;
+					}
+				}
+			}
+			//if no cake or door present in the room
+			if (isSolution == false) {
+				System.out.println("The Cake is a Lie.");
+			}
+			//adding possible moves in north, south, east, west locations of the current spot of Kirby
+			//Kirby's move are kept track in dequeue
+			//loop stops when cake is found
+			while (stack.size() >= 2) {
+				int curCol = stack.pop(); //y
+				int curRow = stack.pop(); //x
+				//System.out.println(curRow + " " + curCol);
+				dequeue.add(curRow);
+				dequeue.add(curCol);
+				if (curRow > (currentRoom-1)*rows) {
+					if (map[curRow-1][curCol].equals("C") || map[curRow-1][curCol].equals("|")) {
+						cakeRow = curRow-1;
+						cakeCol = curCol;
+						break;
+					} else if (map[curRow-1][curCol].equals("1")) {
+						stack.push(curRow-1);
+						stack.push(curCol);
+						map[curRow-1][curCol] = "2";
+					}
+				}
+				if (curRow-((currentRoom-1)*rows) < rows-1) {
+					if (map[curRow+1][curCol].equals("C") || map[curRow+1][curCol].equals("|")) {
+						cakeRow = curRow+1;
+						cakeCol = curCol;
+						break;
+					}else if (map[curRow+1][curCol].equals("1")) {
+						stack.push(curRow+1);
+						stack.push(curCol);
+						map[curRow+1][curCol] = "2";
+					}
+				}
+				if (curCol < cols-1) {
+					if (map[curRow][curCol+1].equals("C") || map[curRow][curCol + 1].equals("|")) {
+						cakeRow = curRow;
+						cakeCol = curCol+1;
+						break;
+					} else if (map[curRow][curCol + 1].equals("1")) {
+						stack.push(curRow);
+						stack.push(curCol+1);
+						map[curRow][curCol+1] = "2";
+					}
+				}
+				if (curCol > 0) {
+					if (map[curRow][curCol-1].equals("C") || map[curRow][curCol-1].equals("|")) {
+						cakeRow = curRow;
+						cakeCol = curCol - 1;
+						break;
+					} else if (map[curRow][curCol-1].equals("1")) {
+						stack.push(curRow);
+						stack.push(curCol-1);
+						map[curRow][curCol-1] = "2";
+					}
+				}
+			}
+			
+			//calls optimal path if cake and door exists
+			if (isSolution) {
+				findOptimalPath(map, cakeRow, cakeCol);
+			}
+			//calls to print solution if every room has an optimal path
+			if (isSolution) {
+				if (currentRoom == rooms) {
+					System.out.println("solution runtime: " + (System.currentTimeMillis()-locationStart));
+					if (!coordBasedOutput) {
+						printSolution(map);
+					}
+					
+				}
+			}
+			
+		} catch(Exception e) {
+			System.out.println();
+			System.out.println(e);
 		}
 	}
+	
+	//Takes in all the locations visited by Kirby and removes all moves that aren't optimal
+	public static void findOptimalPath(String map[][], int cakeRow, int cakeCol) {
+		
+		//finding optimal path by removing non optimal moves
+		int index = dequeue.size();
+		int curRow = cakeRow;
+		int curCol = cakeCol;
+		while (index >= 2) {
+			int tempRow1 = dequeue.get(index-2);
+			int tempCol1 = dequeue.get(index-1);
+			//System.out.println(curRow + " " + curCol + " " + tempRow1 + " " + tempCol1);
+			if (curRow == tempRow1 && curCol == tempCol1-1 || curRow == tempRow1 && curCol == tempCol1+1 ||
+					curRow == tempRow1+1 && curCol == tempCol1 || curRow == tempRow1-1 && curCol == tempCol1) {
+				curRow = tempRow1;
+				curCol = tempCol1;
+			} else {
+				dequeue.remove(index-1);
+				dequeue.remove(index-2);
+			}
+			index -= 2;
+		}
+		
+		//checking if solution exists to see whether to continue to printing the solution
+		if (dequeue.size() < 2) {
+			isSolution = false;
+		}
+
+		//printing the coordinates of the optimal path
+		if (isSolution) {
+			for (int i = 2; i < dequeue.size(); i+=2) {
+				int temp = dequeue.get(i);
+				int temp2 = dequeue.get(i+1);
+				map[temp][temp2] = "+";
+				if (coordBasedOutput) {
+					int roomAdjust = temp / rows;
+					System.out.println("+" + " " + (temp-(roomAdjust*rows)) + " " + temp2);
+				}
+				
+				
+			}
+		} else { //printing that there was no optimal path to the cake
+			System.out.println("The Cake is a Lie, cake exists but no solution");
+		}
+		
+	}
+	
+	//prints the solution map with the optimal path and whether stack/queue approach was used
 	public static void printSolution(String map[][]) {
 		//printing solution map
 		for (int i = 0; i < map.length; i++) {
@@ -313,63 +480,12 @@ public class p1 {
 			System.out.println();
 		}
 		
-	}
-	
-	/*
-	//true if 1-3 is shorter, false if 2-3 is shorter
-	static int temp = dequeue.size()-1;
-	public static boolean shorterDistance(int x1, int y1, int x2, int y2, int x3, int y3, int iter) {
-		int distance1 = (int) Math.pow((Math.pow(x1-x3, 2) + Math.pow(y1-y3, 2)), 0.5);
-		int distance2 = (int) Math.pow((Math.pow(x2-x3, 2) + Math.pow(y2-y3, 2)), 0.5);
-		
-		if (distance1 == distance2) {
-			iter++;
-			x3 = dequeue.get(dequeue.size()-2*iter);
-			y3 = dequeue.get(dequeue.size()-1*iter);
-			return shorterDistance(x1, y1, x2, y2, x3, y3, iter);
-		}
-		return distance1 < distance2;
-	}
-	*/
-	
-
-	
-	
-	//optimal path using distance and arraylist from kirby
-	
-	/*
-	//removing non optimal moves from dequeue
-	int x = 0;
-	while (x < dequeue.size() - 5) {
-		int curRow = dequeue.get(x);
-		int curCol = dequeue.get(x+1);
-		int tempRow1 = dequeue.get(x+2);
-		int tempCol1 = dequeue.get(x+3);
-		int tempRow2 = dequeue.get(x+4);
-		int tempCol2 = dequeue.get(x+5);
-		
-		//determine if possible move from curRow and curCol to next step
-		if (!(curRow == tempRow1 && curCol == tempCol1-1 || curRow == tempRow1 && curCol == tempCol1+1 ||
-				curRow == tempRow1+1 && curCol == tempCol1 || curRow == tempRow1-1 && curCol == tempCol1)) {
-			dequeue.remove(x+2);
-			dequeue.remove(x+2);
-		} else if (!(curRow == tempRow2 && curCol == tempCol2-1 || curRow == tempRow2 && curCol == tempCol2+1 ||
-				curRow == tempRow2+1 && curCol == tempCol2 || curRow == tempRow2-1 && curCol == tempCol2)) {
-			x+=2;
+		if (queueBased) {
+			 System.out.println("Solution with Queue");
 		} else {
-			if (shorterDistance(tempRow1, tempCol1, tempRow2, tempCol2, cakeRow, cakeCol, 0)) {
-				dequeue.remove(x+4);
-				dequeue.remove(x+4);
-				x+=2;
-			} else {
-				dequeue.remove(x+2);
-				dequeue.remove(x+2);
-				x+=2;
-			}
+			 System.out.println("Solution with Stack");
 		}
 		
 	}
-	*/
-	
 }
 
